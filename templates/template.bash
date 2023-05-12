6f682dbc5e1be01cb545c79743316d4d113c2c9c
Francis M <francism@destinatech.com>
2023-05-12 04:28:59 +0100

    Add templates/template.bash
    - Add new template file for bash(1) scripts.

---
+++ templates/template.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2022 E. F. M. trading as Destinatech
+# All rights reserved.
+#
+# See top-level LICENSE.txt file for lecensing terms; alternativel
+# they can be viewed online at:
+#
+#   <https://destinatech.com/licensing/github>
+#
+# Author: AUTHOR_NAME <AUTHOR_EMAIL>
+# Last-Modified: 2023/05/12 04:23-27 BST
+##
+
+
+
+##
+# vim: ts=2 sw=2 et fdm=marker :
+##
